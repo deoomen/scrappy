8a1bf1863f63fc3980ece35195cfcfce97dba4e3
deoomen <deoomen@protonmail.com>
2023-01-10 20:56:30 +0100

    feat: :sparkles: dynamically create ScrapMe instances & scrap

--- app/scrappy.py
+++ app/scrappy.py
@@ -1,7 +1,14 @@
+from os import walk
 from ScrapMe import *
 
 if __name__ == '__main__':
-    print('Hello from Python!')
+    print('Scrapping started')
 
-    s = Sample1()
-    s.scrap()
+    filenames = next(walk('./ScrapMe'), (None, None, []))[2]
+    filenames.remove('__init__.py')
+    filenames.remove('ScrapMeInterface.py')
+
+    for filename in filenames:
+        klass = globals()[filename[0:-3]]
+        s = klass()
+        s.scrap()
