ec0b32fd74fcb7df9633e89de6803dc8becd7388
deoomen <deoomen@protonmail.com>
2023-01-10 21:02:11 +0100

    feat: :sparkles: add stdout log handler & try catch exceptions when scrapping

--- app/scrappy.py
+++ app/scrappy.py
@@ -1,17 +1,32 @@
-from datetime import datetime
+import logging
+import sys
 from os import walk
 from ScrapMe import *
 
 if __name__ == '__main__':
-    print('Scrapping started at', datetime.now())
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+    logging.info('Scrapping started')
 
     filenames = next(walk('./ScrapMe'), (None, None, []))[2]
     filenames.remove('__init__.py')
     filenames.remove('ScrapMeInterface.py')
 
     for filename in filenames:
-        klass = globals()[filename[0:-3]]
+        classname = filename[0:-3]
+        klass = globals()[classname]
         s = klass()
-        s.scrap()
 
-    print('Scrapping ended at', datetime.now())
+        try:
+            s.scrap()
+        except Exception as exception:
+            logging.error('An exception occured during "' + classname + '" scrapping')
+
+
+    logging.info('Scrapping ended')
