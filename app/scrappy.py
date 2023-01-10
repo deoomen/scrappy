001cf6705982f54b7eaedf251959006e81856fd8
deoomen <deoomen@protonmail.com>
2023-01-10 20:57:31 +0100

    feat: :sparkles: print begining & ending time

--- app/scrappy.py
+++ app/scrappy.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from os import walk
 from ScrapMe import *
 
 if __name__ == '__main__':
-    print('Scrapping started')
+    print('Scrapping started at', datetime.now())
 
     filenames = next(walk('./ScrapMe'), (None, None, []))[2]
     filenames.remove('__init__.py')
@@ -12,3 +13,5 @@ if __name__ == '__main__':
         klass = globals()[filename[0:-3]]
         s = klass()
         s.scrap()
+
+    print('Scrapping ended at', datetime.now())
