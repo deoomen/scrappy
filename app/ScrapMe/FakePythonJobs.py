2ec2f467cf16297a00c9c5a14d663e4d50237e6e
deoomen <deoomen@protonmail.com>
2023-01-11 20:23:59 +0100

    feat: :sparkles: add another more real example of scrapping

---
+++ app/ScrapMe/FakePythonJobs.py
@@ -0,0 +1,23 @@
+from ScrapMe.ScrapMe import ScrapMe
+
+class FakePythonJobs(ScrapMe):
+
+    def scrap(self) -> None:
+        soup = self._loadPage('https://realpython.github.io/fake-jobs/')
+
+        jobItems = soup.find(id="ResultsContainer").find_all("div", class_="card-content")
+
+        for index, jobItem in enumerate(jobItems):
+            jobTitle = jobItem.find('h2', class_='title').text
+            jobCompany = jobItem.find('h3', class_='company').text
+            jobLocation = jobItem.find('p', class_='location').text
+            jobDate = jobItem.find('time').text
+
+            print('Job ', index)
+            print('Title:', jobTitle)
+            print('Company:', jobCompany)
+            print('Location:', jobLocation)
+            print('Date:', jobDate)
+            print()
+
+        # do some other things
--- app/ScrapMe/ScrapMe.py
+++ app/ScrapMe/ScrapMe.py
@@ -1,4 +1,11 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 class ScrapMe:
 
     def scrap(self) -> None:
         raise NotImplementedError('Method "scrap" must be implemented')
+
+    def _loadPage(self, url: str) -> BeautifulSoup:
+        html = urlopen(url).read().decode("utf-8")
+
+        return BeautifulSoup(html, "html.parser")
