00e0f5564c435112898b6d12326df773dea6a0c3
deoomen <deoomen@protonmail.com>
2023-01-11 19:39:14 +0100

    feat: :sparkles: support for input args, make main class, split logic to methods

--- app/scrappy.py
+++ app/scrappy.py
@@ -3,30 +3,45 @@ import sys
 from os import walk
 from ScrapMe import *
 
+class Scrappy:
+
+    def __init__(self) -> None:
+        self.initLoggers()
+
+    def initLoggers(self) -> None:
+        # STDOUT logger
+        root = logging.getLogger()
+        root.setLevel(logging.DEBUG)
+
+        stdoutHandler = logging.StreamHandler(sys.stdout)
+        stdoutHandler.setLevel(logging.DEBUG)
+        stdoutFormatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        stdoutHandler.setFormatter(stdoutFormatter)
+
+        root.addHandler(stdoutHandler)
+
+    def run(self, scrapMeList = []) -> None:
+        logging.info('[SCRAPPY] Scrapping started')
+
+        # when no scrap list given, load&run all scrap files
+        if not scrapMeList:
+            scrapMeList = next(walk('./ScrapMe'), (None, None, []))[2]
+            scrapMeList.remove('__init__.py')
+            scrapMeList.remove('ScrapMeInterface.py')
+            scrapMeList = [n[:-3] for n in scrapMeList]
+
+        for classname in scrapMeList:
+            try:
+                klass = globals()[classname]
+                scrapMe = klass()
+                scrapMe.scrap()
+            except Exception:
+                logging.exception('[SCRAPPY] An exception occured during "' + classname + '" scrapping')
+
+        logging.info('[SCRAPPY] Scrapping ended')
+
 if __name__ == '__main__':
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    root.addHandler(handler)
+    scrapMeList = sys.argv[1:]
 
-    logging.info('Scrapping started')
-
-    filenames = next(walk('./ScrapMe'), (None, None, []))[2]
-    filenames.remove('__init__.py')
-    filenames.remove('ScrapMeInterface.py')
-
-    for filename in filenames:
-        classname = filename[0:-3]
-        klass = globals()[classname]
-        s = klass()
-
-        try:
-            s.scrap()
-        except Exception as exception:
-            logging.error('An exception occured during "' + classname + '" scrapping')
-
-
-    logging.info('Scrapping ended')
+    scrappy = Scrappy()
+    scrappy.run(scrapMeList)
