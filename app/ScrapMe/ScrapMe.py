6263e4bb1b3f464c8443c39719f6603c1bbb663d
deoomen <deoomen@protonmail.com>
2023-01-23 18:57:25 +0100

    feat: :sparkles: use random user agents for requests

--- app/ScrapMe/ScrapMe.py
+++ app/ScrapMe/ScrapMe.py
@@ -1,12 +1,29 @@
+import requests
+import requests_random_user_agent
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
 
 class ScrapMe:
+    __requests = None
 
     def scrap(self) -> None:
         raise NotImplementedError('Method "scrap" must be implemented')
 
-    def _loadPage(self, url: str) -> BeautifulSoup:
-        html = urlopen(url).read().decode("utf-8")
+    def _startSession(self) -> None:
+        self.__requests = requests.Session()
 
-        return BeautifulSoup(html, "html.parser")
+    def _loadPage(self, url: str) -> BeautifulSoup:
+        if None is self.__requests:
+            self.__requests = requests
+
+        response = self.__requests.get(url)
+        # contentType = response.headers['Content-Type'].lower().strip()
+        parser = 'html.parser'
+
+        # if 'text/html' in contentType:
+        #     parser = 'lxml'
+        # elif 'application/json' in contentType:
+        #     parser = 'html.parser'
+        # else:
+        #     raise Exception('Unexpected Content-Type', contentType)
+
+        return BeautifulSoup(response.content, parser)
