834784df8975ff96e305d0978e4140fdff6b832a
deoomen <deoomen@protonmail.com>
2023-02-12 12:07:10 +0100

    feat: :sparkles: wrap email send in try-except block

--- app/tools/NotifierEmail.py
+++ app/tools/NotifierEmail.py
@@ -16,21 +16,25 @@ class NotifierEmail:
         self.__smtp = smtplib.SMTP('localhost')
 
     def send(self, title: str, content: str, contentType: str = 'plain') -> None:
-        envs = dotenv_values(ENV_FILE)
+        try:
+            envs = dotenv_values(ENV_FILE)
 
-        sender = envs['EMAIL_SENDER']
-        recipients = envs['EMAIL_RECIPIENTS'].split(',')
-        recipients = list(filter(None, recipients))
+            sender = envs['EMAIL_SENDER']
+            recipients = envs['EMAIL_RECIPIENTS'].split(',')
+            recipients = list(filter(None, recipients))
 
-        if 0 == len(recipients):
-            logging.warning('[SCRAPPY][NotifierEmail] Empty recipients list. Are you really want to send any e-mail?')
-            return
+            if 0 == len(recipients):
+                logging.warning('[SCRAPPY][NotifierEmail] Empty recipients list. Are you really want to send any e-mail?')
+                return
 
-        message = MIMEMultipart('alternative')
-        message['Subject'] = title
-        message['From'] = sender
-        message['To'] = ','.join(recipients)
-        message.attach(MIMEText(content, contentType))
+            message = MIMEMultipart('alternative')
+            message['Subject'] = title
+            message['From'] = sender
+            message['To'] = ','.join(recipients)
+            message.attach(MIMEText(content, contentType))
 
-        self.__smtp.sendmail(sender, recipients, message.as_string())
-        self.__smtp.quit()
+            self.__smtp.sendmail(sender, recipients, message.as_string())
+            self.__smtp.quit()
+        except Exception as exception:
+            logging.exception('[SCRAPPY][NotifierEmail] Sending e-mail failed')
+            raise exception
